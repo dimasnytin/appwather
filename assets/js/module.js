dddea45b239da2c8b52b702353b0c794cf8f64be
dimasnytin <60892736+dimasnytin@users.noreply.github.com>
2023-03-31 09:28:37 +0300

    add module js date

--- assets/js/module.js
+++ assets/js/module.js
@@ -1,6 +1,34 @@
-/**
- * @license MIT
- * @fileoverview All module functions
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+'use strict';
+
+export const weekDayNames = [
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+  "Воскресенье"
+];
+
+export const monthNames = [
+  "Январь",
+  "Февраль",
+  "Март",
+  "Апрель",
+  "Май",
+  "Июнь",
+  "Июль",
+  "Август",
+  "Сентябрь",
+  "Октябрь",
+  "Ноябрь",
+  "Декабрь"
+];
+
+export const getDate = function (dateUnix, timezone) {
+  const date = new Date((dateUnix + timezone) * 1000);
+  const weekDayName = weekDayNames[date.getUTCDay()];
+  const monthName = monthNames[date.getUTCMonth()];
+
+  return `${weekDayName} ${date.getUTCDay()}, ${monthName}`;
+}
