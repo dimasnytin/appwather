580585ff5b557f274a04eeec0ad7129128b040f2
dimasnytin <60892736+dimasnytin@users.noreply.github.com>
2023-03-31 09:14:12 +0300

    add api js ready

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,9 +1,3 @@
-/**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
 
 'use stirct';
 
@@ -23,9 +17,23 @@ export const url = {
   currentWeather(lat, lon){
     return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
   },
-
-  // https://youtu.be/QMwyNnjAils?t=6523
-  // https://www.postman.com/galactic-resonance-776965/workspace/openweather-free/request/24215605-d9a09c0b-9a94-4167-86b0-5a4095eb5e47
-
-  // https://home.openweathermap.org/api_keys
+  forecast(lat, lon){
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+  },
+  airPollution(lat, lon){
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+  },
+  reverseGeo(lat, lon){
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+  },
+  geo(query){
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+  }
 }
+
+
+
+
+// https://youtu.be/QMwyNnjAils?t=6523
+// https://www.postman.com/galactic-resonance-776965/workspace/openweather-free/request/24215605-d9a09c0b-9a94-4167-86b0-5a4095eb5e47
+// https://home.openweathermap.org/api_keys
