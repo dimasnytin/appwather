7e0f7c5ec5f16212b823ea250c69f5e3a724bf48
dimasnytin <60892736+dimasnytin@users.noreply.github.com>
2023-04-01 14:58:20 +0300

    add js location

--- assets/js/route.js
+++ assets/js/route.js
@@ -1,6 +1,33 @@
-/**
- * @license MIT
- * @fileoverview Menage all routes
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+'use strict';
+
+import { updateWeather, error404 } from './app.js';
+const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474"
+
+const currentLocation = function () {
+  
+}
+
+const seachedLocation = query => {
+
+}
+
+const routes = new Map (
+  [
+    ["/current-location", currentLocation],
+    ["/weather", currentLocation],
+  ]
+);
+
+const checkHash = function () {
+  
+}
+
+window.addEventListener("hashchange", checkHash);
+
+window.addEventListener("load", function () {
+  if (window.location.hash){
+    this.window.location.hash = "#/current-location"
+  }else{
+    
+  }
+})
